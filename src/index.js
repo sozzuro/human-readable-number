2be3308d1fe0cdb2231e8f0c4c14a35732978f7d
Denis Radivil <sozzuro@gmail.com>
2020-10-20 12:11:08 +0300

    feat: complete human-readable task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function toReadable (number) {
+    const digits = number%10;
   
-}
+    if (number < 20) {
+      return arrayDigits[number];
+    } else if(number >= 20 && number < 100) {
+      return arrayTens[Math.floor(number/10)] + (!digits ? '' : ' ' + arrayDigits[digits]);
+    } else if(number >= 100 && number < 1000) {
+      return arrayDigits[Math.floor(number/100)] + ' hundred' + (number % 100 == 0 ? '' : ' ' + toReadable(number % 100));
+    }
+  
+    return 'error';
+  }
+  
+  const arrayDigits = ['zero','one','two','three','four', 'five','six','seven','eight','nine','ten','eleven','twelve','thirteen', 'fourteen','fifteen','sixteen', 'seventeen','eighteen','nineteen']
+  const arrayTens = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
